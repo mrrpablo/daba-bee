02b4c732baee9d551865c3e9c616fed6d660eb0f
Precious Bassey <precijagz480@gmail.com>
2023-02-03 09:28:12 -0800

    first commit

---
+++ daba.js
@@ -0,0 +1,28 @@
+const bar = document.querySelector(".fa-bars");
+const menu = document.querySelector(".menu");
+
+bar.addEventListener("click", () => {
+  menu.classList.toggle("show-menu");
+});
+
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes(){
+    const triggerButton = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerButton) {
+            box.classList.add('show')
+        }else{
+            box.classList.remove('show')
+        }
+
+    })
+    setTimeout(boxes, 2000)
+}
